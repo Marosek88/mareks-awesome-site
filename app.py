ca57d89cfcd6b72b622bf5309589fc53609396be
mkubiak <mkubiak@shuttertsock.com>
2021-06-17 21:54:42 +0100

    initial added templates

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_PATH = os.path.join(APP_PATH, 'templates/')
+STATIC_PATH = os.path.join(APP_PATH, 'static/')
+app = Flask(__name__, template_folder=TEMPLATE_PATH, static_folder=STATIC_PATH)
+
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def index():
+	return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app.run()
